070ba6e2e825e8c918c5823a186fb576e82c7e4c
Gabriela <gabisgamba@gmail.com>
2024-10-08 09:49:49 -0300

    Feature:userRoutes

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import userController from "../controllers/userController";
+import permissionVerify from "../middleware/authentication";
+const router = express.Router();
+
+router.get("/", permissionVerify, userController.getUsers);
+router.post("/", userController.createUser);
+router.patch("/:id", permissionVerify, userController.updateUser);
+router.delete("/logout", permissionVerify, userController.logoutUser);
+router.delete("/:id", permissionVerify, userController.deleteUser);
+router.post("/login", userController.authenticateUser);
+
+export default router;
