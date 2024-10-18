771c3692a50a8ed84fdbd2167d8f65ef5556ebd9
Denner Willian <dennerwillian01@gmail.com>
2024-10-18 10:58:02 -0300

    feature: Implemented ToolNode to make AI tool execution more efficient

--- src/utils/ai-assistant.ts
+++ src/utils/ai-assistant.ts
@@ -2,12 +2,11 @@ import { ChatOpenAI } from '@langchain/openai';
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { AIMessage, HumanMessage, ToolMessage } from '@langchain/core/messages';
 import {
-  START,
-  END,
   MessagesAnnotation,
   StateGraph,
   Annotation,
 } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import redis from '../models/redis';
 
 import { toolsByName, tools } from './ai-tools';
@@ -21,13 +20,14 @@ const prompt = ChatPromptTemplate.fromMessages([
     - Antes de realizar qualquer ação, identificar corretamente todos os **dados necessários**. Isso pode incluir procurar IDs de colunas, usuários, ou qualquer outro elemento.
     - Se algum dado estiver faltando ou não for especificado, você deve **solicitar mais informações** ao usuário antes de proceder. **Não execute nenhuma ferramenta enquanto informações faltantes não forem fornecidas**.
     - Após reunir todas as informações, você deve **executar a ação solicitada** de forma eficiente e correta.
-    - O usuário nunca saberá o ID das coisas, apenas nome, ou descrição e afins. Então **não exija esse tipo de informação dele**. Se você precisar do ID de algo que o usuário só forneceu o nome ou descriçao, use uma ferramenta que faça uma busca geral e irá te retornar o JSON e extraia o campo ID que você encontrar que se relaciona com o que o usuário forneceu como informação na consulta e utilize isso para dar prosseguimento ao que você precisa.
+    - O usuário nunca saberá o ID das coisas, apenas nome, ou descrição e afins. Então **não exija esse tipo de informação dele**.
+    - Você pode precisar de dados de uma ferramenta especifica para conseguir passar como argumento para outras ferramentas e isso não tem problema.
 
     **Sempre siga estas etapas para qualquer solicitação**:
     1. **Identifique a ação necessária** com base no pedido do usuário.
     2. **Liste todas as informações e dados necessários** (como IDs de colunas, cards, usuários, etc.) para realizar a ação.
     3. **Solicite os dados faltantes ao usuário**. **Não acione ferramentas ou realize buscas automáticas** até que todos os dados necessários sejam fornecidos.
-    4. **Execute a ação** somente após ter reunido todas as informações e dados necessários.
+    4. **Execute a ação** somente após ter reunido todas as informações, dados necessários.
     5. **Seja claro em suas respostas ao usuário** sobre o que foi feito ou o que está faltando para completar a solicitação.
 
     Exemplos de ações que você pode realizar:
@@ -54,18 +54,30 @@ const GraphAnnotation = Annotation.Root({
   boardID: Annotation<string>(),
 });
 
-const callMode = async (state: typeof GraphAnnotation.State) => {
+const toolNodeForGraph = new ToolNode(tools)
+
+const shouldContinue = (state: typeof GraphAnnotation.State) => {
+  const { messages } = state;
+  const lastMessage = messages[messages.length - 1];
+  if ("tool_calls" in lastMessage && Array.isArray(lastMessage.tool_calls) && lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+  return "__end__";
+}
+
+const callModel = async (state: typeof GraphAnnotation.State) => {
   const chain = prompt.pipe(llm);
   const response = await chain.invoke({ messages: state.messages, userID: state.userID, boardID: state.boardID });
   return { messages: response };
 };
 
-const workflow = new StateGraph(GraphAnnotation)
-  .addNode("model", callMode)
-  .addEdge(START, "model")
-  .addEdge("model", END);
-
-const app = workflow.compile();
+const app = new StateGraph(GraphAnnotation)
+  .addNode("agent", callModel)
+  .addNode("tools", toolNodeForGraph)
+  .addEdge("__start__", "agent")
+  .addConditionalEdges("agent", shouldContinue)
+  .addEdge("tools", "agent")
+  .compile();
 
 const getRedisKey = (userID: string, boardID: string) => `kanban:${userID}:${boardID}:messages`;
 
@@ -98,12 +110,8 @@ export const ChatBot = async (query: string, user_id: string, board_id: string) 
     let response = await app.invoke(input);
     console.log(response);
     console.log("\n************************************************************************************\n")
-    if (!response.messages[response.messages.length-1].tool_calls.length) {
-      const aiFinalMessage = response.messages.at(-1).content;
-      await saveMessageHistory(user_id, board_id, response.messages);
-      return aiFinalMessage;
-    } else {
-      for (const toolCall of response.messages[response.messages.length-1].tool_calls) {
+    if (response.messages[response.messages.length - 1].tool_calls.length > 0) {
+      for (const toolCall of response.messages[response.messages.length - 1].tool_calls) {
         const selectedTool = toolsByName[toolCall.name as keyof typeof toolsByName];
         if (!selectedTool) {
           console.error(`Tool não encontrada: ${toolCall.name}`);
@@ -132,7 +140,11 @@ export const ChatBot = async (query: string, user_id: string, board_id: string) 
 
       await saveMessageHistory(user_id, board_id, filteredFinalMessages);
 
-      if(aiFinalMessage === "") aiFinalMessage = "Desculpe, não consegui processar sua solicitação."
+      if (aiFinalMessage === "") aiFinalMessage = "Desculpe, não consegui processar sua solicitação."
+      return aiFinalMessage;
+    } else {
+      const aiFinalMessage = response.messages.at(-1).content;
+      await saveMessageHistory(user_id, board_id, response.messages);
       return aiFinalMessage;
     }
   } catch (error) {
