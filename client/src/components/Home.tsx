99b5dc9d4f1dff44cdc54c346c98cddb85f39cf4
Kaliane Marques <Kalianemarques@outlook.com>
2024-10-21 19:43:11 -0300

    ajuste na função de adicionar board

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -22,30 +22,31 @@ const Home: React.FC<HomeProps> = ({ openBoard }) => {
     const [projects, setProjects] = useState<Project[]>([]);
     const url = process.env.REACT_APP_API_URL;
 
-    useEffect(() => {
-        async function fetchProjects() {
-            try {
-                const response = await fetch(`${url}/board/myBoards`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    credentials: 'include',
-                });
-                const result = await response.json();
-                if (Array.isArray(result.data)) {
-                    setProjects(result.data);
-                } else if (result.data === "Você não está em nenhum quadro.") {
-                    setProjects([]);
-                } else {
-                    console.error('Resposta inesperada', result);
-                }
-            } catch (error) {
-                console.error('Erro ao buscar projetos', error);
+    const getMyBoards = async()=>{
+        try {
+            const response = await fetch(`${url}/board/myBoards`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+            const result = await response.json();
+            if (Array.isArray(result.data)) {
+                setProjects(result.data);
+            } else if (result.data === "Você não está em nenhum quadro.") {
+                setProjects([]);
+            } else {
+                console.error('Resposta inesperada', result);
             }
+        } catch (error) {
+            console.error('Erro ao buscar projetos', error);
         }
+    }
+    useEffect(() => {
+        getMyBoards();
 
-        fetchProjects();
+        getMyBoards();
     }, [url]);
 
     const handleAddProject = (newProject: Project) => {
@@ -65,24 +66,33 @@ const Home: React.FC<HomeProps> = ({ openBoard }) => {
         setNameProject(e.target.value);
     };
 
-    const handleConfirmClick = (event: React.MouseEvent) => {
+    const handleConfirmClick = async (event: React.MouseEvent) => {
         const newProject: Project = {
             name: nameProject,
             description: "",
         };
-
-        handleAddProject(newProject);
-        fetch(`${url}/board/create`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify(newProject),
-        });
-
-        setNameProject("");
-        setIsDialogOpen(false);
+    
+        try {
+            const response = await fetch(`${url}/board/create`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify(newProject),
+            });
+    
+            if (!response.ok) {
+                console.error('Erro ao criar o projeto');
+                return;
+            }
+            await getMyBoards();
+            setNameProject("");
+            setIsDialogOpen(false);
+    
+        } catch (error) {
+            console.error('Erro ao criar o projeto:', error);
+        }
     };
 
     return (
